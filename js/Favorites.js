7e6bcd34df8e36d0eca08a9740ed5e5970429608
Renan Kimura <kimura@live.it>
2023-09-11 11:34:12 -0300

    tratamento para usuário repetido

--- js/Favorites.js
+++ js/Favorites.js
@@ -15,8 +15,6 @@ export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
-        GithubUser.search('diego3g')
-            .then(user => console.log(user))
     }
 
     load() {
@@ -28,8 +26,13 @@ export class Favorites {
     }
     async add(username){
         try{
+            const userExists = this.entries.find(entry =>entry.login === username)
+            if(userExists){
+                throw new Error('Usuário já cadastrado')
+            }
+
             const user = await GithubUser.search(username)
-            console.log(user)
+            // console.log(user)
 
             if(user.login === undefined){
                 throw new Error('Usuário não encontrado')
